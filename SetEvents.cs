f07a353deccf484dd5bd11b059216f631d9aeaf1
Tabula <MrZhnex@ya.ru>
2020-07-02 23:27:49 +0300

    Upload

---
+++ SetEvents.cs
@@ -0,0 +1,48 @@
+﻿using EXILED;
+using EXILED.Extensions;
+using Mirror;
+using UnityEngine;
+
+namespace VanHeli
+{
+    public class SetEvents : NetworkBehaviour
+    {
+        public MTFRespawn mtfRespawn;
+        public ChopperAutostart mtf_a;
+
+        public void OnRemoveAdminCommand(ref RACommandEvent ev)
+        {
+            switch (ev.Command)
+            {
+                case "heli":
+                    if (this.mtf_a == null)
+                    {
+                        this.mtf_a = Object.FindObjectOfType<ChopperAutostart>();
+                    }
+                    if (this.mtf_a == null)
+                    {
+                        ev.Sender.RAMessage("Failed to hook ChopperAutostart, try again after round start", false);
+                        return;
+                    }
+                    this.mtf_a.SetState(!this.mtf_a.NetworkisLanded);
+                    ev.Sender.RAMessage(this.mtf_a.NetworkisLanded ? "Landing..." : "Flying away");
+                    return;
+                case "van":
+                    if (this.mtfRespawn == null)
+                    {
+                        this.mtfRespawn = Object.FindObjectOfType<MTFRespawn>();
+                    }
+                    if (this.mtfRespawn == null)
+                    {
+                        ev.Sender.RAMessage("Failed to hook MTFRespawn, try again after round start", false);
+                        return;
+                    }
+                    mtfRespawn.RpcVan();
+
+                    ev.Sender.RAMessage("Van called");
+                    return;
+            }
+        }
+
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,25 @@
+﻿using EXILED;
+
+namespace VanHeli
+{
+    public class Settings : Plugin
+    {
+        public override string getName => "VanHeli";
+        public SetEvents SetEvents;
+
+        public override void OnEnable()
+        {
+            SetEvents = new SetEvents();
+            Events.RemoteAdminCommandEvent += SetEvents.OnRemoveAdminCommand;
+            Log.Info(getName + " on");
+        }
+
+        public override void OnDisable()
+        {
+            Events.RemoteAdminCommandEvent -= SetEvents.OnRemoveAdminCommand;
+            Log.Info(getName + " off");
+        }
+
+        public override void OnReload() { }
+    }
+}
